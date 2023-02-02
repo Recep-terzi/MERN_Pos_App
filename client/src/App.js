9e408ae70aa512a97795bcb1c4ff70cb2ecec8d6
Recep Terzi <recepterzi-67@hotmail.com>
2023-02-02 21:34:44 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import CartTotals from "./components/CartTotals/CartTotals";
+import Categories from "./components/Categories/Categories";
 import Header from "./components/Header/Header";
+import Products from "./components/Products/Products";
 
 function App() {
   return (
     <>
       <Header />
-      adasds
+      <div className="home  w-[1440px] max-w-[96%] ml-auto mr-auto flex justify-between gap-10 ">
+        <div className="categories  overflow-auto max-h-[calc(100vh-_-112px)] pb-64">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <Products />
+        </div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <CartTotals />
+        </div>
+      </div>
     </>
   );
 }
