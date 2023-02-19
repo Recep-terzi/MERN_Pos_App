930c2afd4c1fff089632b846b349d0057c32a639
Recep Terzi <recepterzi-67@hotmail.com>
2023-02-19 21:52:58 +0300

    mern

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = express();
 const port = 5000;
+dotenv.config();
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => {
+  connect();
   console.log("Example app listenin on port " + port);
 });
