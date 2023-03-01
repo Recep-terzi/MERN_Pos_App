bb0afad207309a6846159010a5df0a015f70a81e
Recep Terzi <recepterzi-67@hotmail.com>
2023-03-01 22:54:52 +0300

    update CRUD

--- api/server.js
+++ api/server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
+const cors = require("cors");
 const port = 5000;
 dotenv.config();
+
+// routes
+
+const categoryRoute = require("./routes/categories.js");
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -13,7 +18,11 @@ const connect = async () => {
   }
 };
 
-app.get("/", (req, res) => res.send("Hello World!"));
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/categories", categoryRoute);
 app.listen(port, () => {
   connect();
   console.log("Example app listenin on port " + port);
